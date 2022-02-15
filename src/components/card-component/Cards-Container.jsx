4c7809a4885198cece106dce4637123fed24730f
hristakiev1 <axsmarine@abv.bg>
2022-02-15 14:48:48 +0200

    Build Card component and Card-Container

--- src/components/card-component/Cards-Container.jsx
+++ src/components/card-component/Cards-Container.jsx
@@ -4,9 +4,10 @@ import "./Cards-Container.css";
 const CardContainer = () => {
   return (
     <div className="card-container">
-      <h2>Check out these Epic Destinations!</h2>
+      <h2 className="card-container__heading">
+        Check out these Epic Destinations!
+      </h2>
       <div className="card-grid">
-        <Card />
         <Card />
         <Card />
       </div>
--- src/components/utilities/Card.jsx
+++ src/components/utilities/Card.jsx
@@ -3,17 +3,19 @@ import "./Card.css";
 
 const Card = () => {
   return (
-    <div className="card-element">
-      <div>
-        <img
-          className="card-image"
-          src="/source-folder/images/img-8.jpg"
-          alt="camels crossing the Sahara desert"
-        />
-        <span className="image-tag">Adventure</span>
-      </div>
-      <div className="card-text">
-        <h2>Explorer the hidden waterfall deep inside the Amazon jungle</h2>
+    <div className="card-board">
+      <div className="card-element">
+        <div className="card__header">
+          <img
+            className="card__image"
+            src="/source-folder/images/img-8.jpg"
+            alt="camels crossing the Sahara desert"
+          />
+          <span className="image-tag">Adventure</span>
+        </div>
+        <div className="card-text">
+          <h2>Explorer the hidden waterfall deep inside the Amazon jungle</h2>
+        </div>
       </div>
     </div>
   );
