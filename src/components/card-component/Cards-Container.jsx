f3a835f04cb21a0f15b35a3de75708a65be24d6a
hristakiev1 <axsmarine@abv.bg>
2022-02-17 17:05:51 +0200

    Card Items and Card Container fine tuning

--- src/components/card-component/Cards-Container.jsx
+++ src/components/card-component/Cards-Container.jsx
@@ -8,41 +8,54 @@ const CardContainer = () => {
         Check out these Epic Destinations!
       </h2>
       <div className="card-grid">
-        <Card
-          image={"/source-folder/images/img-8.jpg"}
-          sutitle={"Adventure"}
-          text={"Explorer the hidden waterfall deep inside the Amazon jungle"}
-        />
-        <Card
-          image={"/source-folder/images/img-4.jpg"}
-          sutitle={"Luxury"}
-          text={"Travel through the island of Bali in a Private Cruise"}
-        />
-        <Card
-          image={"/source-folder/images/img-9.jpg"}
-          sutitle={"Travel"}
-          text={"Cannioynig in Singapore"}
-        />
-        <Card
-          image={"/source-folder/images/img-8.jpg"}
-          sutitle={"Travel"}
-          text={"Cannioynig in Singapore"}
-        />
-        <Card
-          image={"/source-folder/images/img-8.jpg"}
-          sutitle={"Travel"}
-          text={"Cannioynig in Singapore"}
-        />
-        <Card
-          image={"/source-folder/images/img-8.jpg"}
-          sutitle={"Travel"}
-          text={"Cannioynig in Singapore"}
-        />
-        <Card
-          image={"/source-folder/images/img-8.jpg"}
-          sutitle={"Travel"}
-          text={"Cannioynig in Singapore"}
-        />
+        <div className="big-cards">
+          <Card
+            image={"/source-folder/images/img-8.jpg"}
+            sutitle={"Adventure"}
+            text={"Explorer the hidden waterfall deep inside the Amazon jungle"}
+            path={"/services"}
+          />
+          <Card
+            image={"/source-folder/images/img-4.jpg"}
+            sutitle={"Luxury"}
+            text={"Travel through the island of Bali in a Private Cruise"}
+            path={"/services"}
+          />
+        </div>
+        <div className="small-cards">
+          <Card
+            image={"/source-folder/images/img-9.jpg"}
+            sutitle={"Mystery"}
+            text={"Set Sail in the Atlantic Ocean visiting Uncharted Waters"}
+            path={"/services"}
+          />
+          <Card
+            image={"/source-folder/images/img-2.jpg"}
+            sutitle={"Adventure"}
+            text={"Experiance Football on Top of the Himalayan Mountains"}
+            path={"/services"}
+          />
+          <Card
+            image={"/source-folder/images/img-3.jpg"}
+            sutitle={"Adrenalin"}
+            text={"Ride Through Sahara Desesert ot camel`s back"}
+            path={"/services"}
+          />
+          <Card
+            image={"/source-folder/images/img-5.jpg"}
+            sutitle={"Travel"}
+            text={
+              "Go kill yourself jumping from the edge of Cliff and win Darwin Award"
+            }
+            path={"/services"}
+          />
+          <Card
+            image={"/source-folder/images/img-7.jpg"}
+            sutitle={"Travel"}
+            text={"Cannioynig in Singapore"}
+            path={"/services"}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/utilities/Card.jsx
+++ src/components/utilities/Card.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
-const Card = ({ image, sutitle, text }) => {
+const Card = ({ image, sutitle, text, path }) => {
   return (
     <div className="card-element">
-      <div className="card__header">
-        <img className="card__image" src={image} alt="exotic places" />
-        <span className="image-subtitle">{sutitle}</span>
-      </div>
-      <div className="card-text">
-        <h2>{text}</h2>
-      </div>
+      <Link to={path}>
+        <div className="card__header">
+          <img className="card__image" src={image} alt="exotic places" />
+          <span className="image-subtitle">{sutitle}</span>
+        </div>
+        <div className="card-text">
+          <h2>{text}</h2>
+        </div>
+      </Link>
     </div>
   );
 };
