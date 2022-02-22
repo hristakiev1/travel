e0a76d45fc1fc8acaa5021b020ff67fbe247db29
hristakiev1 <axsmarine@abv.bg>
2022-02-22 14:35:27 +0200

    Build Sign In component submit form

--- src/components/Sign-in-Sign-up/Sign-in.jsx
+++ src/components/Sign-in-Sign-up/Sign-in.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../utilities/InputField";
 import "./Sign-in.css";
 
 const SignIn = () => {
+  const [userAuth, setUserAuth] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = userAuth;
+
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setUserAuth({
+      ...userAuth,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <div className="sign-in">
       <h1>I already have an account </h1>
       <p>Sign in with your email and password</p>
-      <form className="sign-in__form" action="submit">
+      <form className="sign-in__form" action="submit" onSubmit={handleSubmit}>
         <InputField
           name={"email"}
           type={"email"}
           placeholder={"User Name"}
-          required={"required"}
+          // required={"required"}
+          onChange={handleChange}
         />
         <InputField
           name={"password"}
           type={"password"}
           placeholder={"Password"}
-          required={"required"}
+          // required={"required"}
+          onChange={handleChange}
         />
 
-        <button className="btn-submit">Sign In</button>
+        <button className="btn-submit" type="submit">
+          Sign In
+        </button>
       </form>
     </div>
   );
